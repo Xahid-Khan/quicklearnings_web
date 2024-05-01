81f1b8b53cb7af38dc28e2c9a99b43a0a370b2f0
Zahid <xahidkhan88@gmail.com>
2024-05-01 09:43:09 +0900

    Revmoved firebase related code from repo

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,16 +1,9 @@
-import { db } from '@/src/config/firebase'
-import { doc, query, setDoc } from 'firebase/firestore'
 import { NextRequest, NextResponse } from 'next/server'
 import { randomUUID } from 'crypto'
 
 export async function POST(req: NextRequest) {
   console.log('here>>>', req.body)
   const data: Topic[] = []
-  // await setDoc(doc(db, 'data', randomUUID()), {
-  //   ...value,
-  //   created_at: Date.now(),
-  //   topicId: '6OEQgraAvYyQpfpdLVYV'
-  // })
 
   return NextResponse.json(data, { status: 200 })
 }
