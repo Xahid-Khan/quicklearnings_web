0ab0d9540094239bc1ec3382720bcfb15401176a
Zahid <xahidkhan88@gmail.com>
2024-05-22 17:01:40 +0900

    Updated the  migration file for DB

--- supabase/migrations/20240501175127_initial_setup.sql
+++ supabase/migrations/20240501175127_initial_setup.sql
@@ -231,42 +231,93 @@ alter table "public"."subject" add column "public" boolean not null default fals
 alter table "public"."topic" add column "public" boolean not null default false;
 
 
-create table
-  public.profile (
-    id uuid not null references auth.users on delete cascade,
-    created_at timestamp with time zone not null default now(),
-    email text not null,
-    first_name text null,
-    last_name text null,
-    description text null,
-    last_login timestamp with time zone null,
-    last_login_ip text null,
-    is_active boolean not null default true,
-    date_of_birth date null,
-    phone bigint null,
-    blocked boolean not null default false,
-    updated_at timestamp with time zone null,
-    constraint profile_pkey primary key (id),
-    constraint profile_id_key unique (id)
-  ) tablespace pg_default;
+create type "public"."roles" as enum ('student', 'teacher', 'admin');
 
-alter table public.profile enable row level security;
-grant select on table "public"."profile" to "anon";
+create table "public"."profile" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone not null default now(),
+    "email" text not null,
+    "first_name" text,
+    "last_name" text,
+    "description" text,
+    "last_login" timestamp with time zone,
+    "last_login_ip" text,
+    "is_active" boolean not null default true,
+    "date_of_birth" date,
+    "phone" bigint,
+    "blocked" boolean not null default false,
+    "updated_at" timestamp with time zone
+);
 
 
--- Make sure that this is after the profile table is created
+alter table "public"."profile" enable row level security;
 
-create or replace function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = ''
-as $$
+CREATE UNIQUE INDEX profile_pkey ON public.profile USING btree (id);
+
+alter table "public"."profile" add constraint "profile_pkey" PRIMARY KEY using index "profile_pkey";
+
+alter table "public"."profile" add constraint "profile_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profile" validate constraint "profile_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+ SET search_path TO ''
+AS $function$
 begin
   insert into public.profile (id, email, first_name, last_name)
   values (new.id, new.email, new.raw_user_meta_data ->> 'first_name',  new.raw_user_meta_data ->> 'last_name');
   return new;
 end;
-$$;
+$function$
+;
+
+grant delete on table "public"."profile" to "anon";
+
+grant insert on table "public"."profile" to "anon";
+
+grant references on table "public"."profile" to "anon";
+
+grant select on table "public"."profile" to "anon";
+
+grant trigger on table "public"."profile" to "anon";
+
+grant truncate on table "public"."profile" to "anon";
+
+grant update on table "public"."profile" to "anon";
+
+grant delete on table "public"."profile" to "authenticated";
+
+grant insert on table "public"."profile" to "authenticated";
+
+grant references on table "public"."profile" to "authenticated";
+
+grant select on table "public"."profile" to "authenticated";
+
+grant trigger on table "public"."profile" to "authenticated";
+
+grant truncate on table "public"."profile" to "authenticated";
+
+grant update on table "public"."profile" to "authenticated";
+
+grant delete on table "public"."profile" to "service_role";
+
+grant insert on table "public"."profile" to "service_role";
+
+grant references on table "public"."profile" to "service_role";
+
+grant select on table "public"."profile" to "service_role";
+
+grant trigger on table "public"."profile" to "service_role";
+
+grant truncate on table "public"."profile" to "service_role";
+
+grant update on table "public"."profile" to "service_role";
+
 
 create or replace trigger on_auth_user_created after insert on auth.users
 for each row execute procedure public.handle_new_user()
