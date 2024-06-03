936bbe60be4c57b3fb87634b7fdfe38c92795601
Zahid <xahidkhan88@gmail.com>
2024-06-04 00:15:58 +0900

    modified the build script to run tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  important: true,
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
   theme: {
     extend: {
       backgroundImage: {
